89279d9f92d13380d713239bcc95ef9c8bf7131d
Vu Luu <cometxiii@gmail.com>
2014-06-26 09:01:39 +0700

    add new DB

--- Rating.sql
+++ Rating.sql
@@ -94,3 +94,105 @@ update Event
 set Rating=@Rating
 where Event_ID=@Event_ID
 end
+
+--trigger Auto-update so luong comment cho ThongTinDuLich
+create trigger Travel_Comm
+on Travel_Comment_Rating
+for insert, update
+as begin
+declare @Travel_ID int
+declare @Comm int
+select @Travel_ID=Travel_ID from inserted
+set @Comm=(select count(TR.Comment_ID) as Total
+from Travel_Comment_Rating TR
+where TR.Travel_ID=@Travel_ID and TR.Content!='')
+
+update ThongTinDuLich
+set CommentNum=@Comm
+where Travel_ID=@Travel_ID
+end
+
+--trigger Auto-update so luong comment cho ThongTinKhachSan
+create trigger Hotel_Comm
+on Hotel_Comment_Rating
+for insert, update
+as begin
+declare @Hotel_ID int
+declare @Comm int
+select @Hotel_ID=Hotel_ID from inserted
+set @Comm=(select count(HR.Comment_ID) as Total
+from Hotel_Comment_Rating HR
+where HR.Hotel_ID=@Hotel_ID and HR.Content!='')
+
+update ThongTinKhachSan
+set CommentNum=@Comm
+where Hotel_ID=@Hotel_ID
+end
+
+--trigger Auto-update so luong comment cho ThongTinNhaHang
+create trigger Rest_Comm
+on Rest_Comment_Rating
+for insert, update
+as begin
+declare @Rest_ID int
+declare @Comm int
+select @Rest_ID=Rest_ID from inserted
+set @Comm=(select count(RR.Comment_ID) as Total
+from Rest_Comment_Rating RR
+where RR.Rest_ID=@Rest_ID and RR.Content!='')
+
+update ThongTinNhaHang
+set CommentNum=@Comm
+where Rest_ID=@Rest_ID
+end
+
+--trigger Auto-update so luong comment cho ThongTinAmThuc
+create trigger AmThuc_Comm
+on AmThuc_Comment_Rating
+for insert, update
+as begin
+declare @AmThuc_ID int
+declare @Comm int
+select @AmThuc_ID=AmThuc_ID from inserted
+set @Comm=(select count(ATR.Comment_ID) as Total
+from AmThuc_Comment_Rating ATR
+where ATR.AmThuc_ID=@AmThuc_ID and ATR.Content!='')
+
+update ThongTinAmThuc
+set CommentNum=@Comm
+where AmThuc_ID=@AmThuc_ID
+end
+
+--trigger Auto-update so luong comment cho Event
+create trigger Event_Comm
+on Event_Comment_Rating
+for insert, update
+as begin
+declare @Event_ID int
+declare @Comm int
+select @Event_ID=Event_ID from inserted
+set @Comm=(select count(ER.Comment_ID) as Total
+from Event_Comment_Rating ER
+where ER.Event_ID=@Event_ID and ER.Content!='')
+
+update Event
+set CommentNum=@Comm
+where Event_ID=@Event_ID
+end
+
+--trigger Auto-update so luong LIKE cho ThongTinDuLich
+create trigger Travel_LikeNum
+on Travel_Like
+for insert, update
+as begin
+declare @Travel_ID int
+declare @Like int
+select @Travel_ID=Travel_ID from inserted
+set @Like=(select count(TL.Like_ID) as Total
+from Travel_Like TL
+where TL.Travel_ID=@Travel_ID and TL.Like_Status=1)
+
+update ThongTinDuLich
+set LikeNum=@Like
+where Travel_ID=@Travel_ID
+end
--- Travel.sql
+++ Travel.sql
@@ -1,4 +1,4 @@
-create database Travel
+﻿create database Travel
 use Travel
 -- use master
 -- drop database Travel
@@ -13,7 +13,7 @@ create table Account
 	Phone varchar(15),
 	Address nvarchar(50),
 	Type bit,
-	Active bit,
+	Active bit
 )
 
 create table ThongTinDuLich
@@ -22,8 +22,20 @@ create table ThongTinDuLich
 	Name nvarchar(20),
 	Address nvarchar(50),
 	Phone varchar(15),
-	Content text,
-	Rating int
+	ShortDescription nvarchar(1000),
+	Content ntext,
+	Rating int,
+	InsertDay char(2),
+	InsertMonth char(3),
+	CommentNum int,
+	CoverPic nvarchar(255),
+	Pic1 nvarchar(255),
+	Pic2 nvarchar(255),
+	Pic3 nvarchar(255),
+	Pic4 nvarchar(255),
+	Pic5 nvarchar(255),
+	Pic6 nvarchar(255),
+	LikeNum int
 )
 
 create table ThongTinKhachSan
@@ -32,8 +44,20 @@ create table ThongTinKhachSan
 	Name nvarchar(20),
 	Address nvarchar(50),
 	Phone varchar(15),
-	Content text,
-	Rating int
+	ShortDescription nvarchar(1000),
+	Content ntext,
+	Rating int,
+	InsertDay char(2),
+	InsertMonth char(3),
+	CommentNum int,
+	CoverPic nvarchar(255),
+	Pic1 nvarchar(255),
+	Pic2 nvarchar(255),
+	Pic3 nvarchar(255),
+	Pic4 nvarchar(255),
+	Pic5 nvarchar(255),
+	Pic6 nvarchar(255),
+	LikeNum int
 )
 
 create table ThongTinNhaHang
@@ -42,24 +66,58 @@ create table ThongTinNhaHang
 	Name nvarchar(20),
 	Address nvarchar(50),
 	Phone varchar(15),
-	Content text,
-	Rating int
+	ShortDescription nvarchar(1000),
+	Content ntext,
+	Rating int,
+	InsertDay char(2),
+	InsertMonth char(3),
+	CommentNum int,
+	CoverPic nvarchar(255),
+	Pic1 nvarchar(255),
+	Pic2 nvarchar(255),
+	Pic3 nvarchar(255),
+	Pic4 nvarchar(255),
+	Pic5 nvarchar(255),
+	Pic6 nvarchar(255),
+	LikeNum int
 )
 
 create table ThongTinAmThuc
 (
 	AmThuc_ID int primary key,
 	Title nvarchar(100),
-	Content text,
-	Rating int
+	Content ntext,
+	Rating int,
+	InsertDay char(2),
+	InsertMonth char(3),
+	CommentNum int,
+	CoverPic nvarchar(255),
+	Pic1 nvarchar(255),
+	Pic2 nvarchar(255),
+	Pic3 nvarchar(255),
+	Pic4 nvarchar(255),
+	Pic5 nvarchar(255),
+	Pic6 nvarchar(255),
+	LikeNum int
 )
 
 create table Event
 (
 	Event_ID int primary key,
 	Title nvarchar(100),
-	Content text,
-	Rating int
+	Content ntext,
+	Rating int,
+	InsertDay char(2),
+	InsertMonth char(3),
+	CommentNum int,
+	CoverPic nvarchar(255),
+	Pic1 nvarchar(255),
+	Pic2 nvarchar(255),
+	Pic3 nvarchar(255),
+	Pic4 nvarchar(255),
+	Pic5 nvarchar(255),
+	Pic6 nvarchar(255),
+	LikeNum int
 )
 
 create table Travel_Comment_Rating
@@ -106,3 +164,15 @@ create table Event_Comment_Rating
 	Content nvarchar(250),
 	Rating int
 )
+
+create table Travel_Like
+(
+	Like_ID int primary key,
+	User_ID uniqueidentifier foreign key references Account(User_ID),
+	Travel_ID int foreign key references ThongTinDuLich(Travel_ID),
+	Like_Status bit
+)
+
+
+97d22396-eeff-4dd8-8768-4a7fb6fde522	phokienhuy	1B3B7A6A202D238CE15BB79C5258F6FB	Phó Kiến Huy	phokienhuy@gmail.com	0989991268	220/14	False	True
+1	ĐÀ LẠT	Lâm Đồng vn	123123123	Ở Việt Nam, Đà Lạt từ lâu đã là một thành phố du lịch nổi tiếng. Khí hậu mát mẻ, tài nguyên thiên nhiên và nhân văn phong phú giúp thành phố mỗi năm thu hút hàng triệu du khách tới thăm viếng và nghỉ dưỡng. Sau một thời gian trầm lắng của thập niên 1980, du lịch Đà Lạt thay đổi mạnh mẽ trong những năm gần đây.	Thành phố Đà Lạt là tỉnh lỵ của tỉnh Lâm Đồng, nằm trên cao nguyên Lâm Viên, thuộc vùng Tây Nguyên, Việt Nam. Từ xa xưa, vùng đất này vốn là địa bàn cư trú của những cư dân người Lạch, người Chil và người Srê thuộc dân tộc Cơ Ho. Cuối thế kỷ 19, khi tìm kiếm một địa điểm để xây dựng trạm nghỉ dưỡng dành cho người Pháp ở Đông Dương, Toàn quyền Paul Doumer đã quyết định chọn cao nguyên Lâm Viên theo đề nghị của bác sỹ Alexandre Yersin, người từng thám hiểm tới nơi đây vào năm 1893. Trong nửa đầu thế kỷ 20, từ một địa điểm hoang vu, những người Pháp đã quy hoạch và xây dựng lên một thành phố xinh đẹp với những biệt thự, công sở, khách sạn và trường học, một trung tâm du lịch và giáo dục của Đông Dương khi đó. Trải qua những khoảng thời gian thăng trầm của hai cuộc chiến tranh cùng giai đoạn khó khăn những thập niên 1970–1980, Đà Lạt ngày nay là một thành phố 211 ngàn dân, đô thị loại I trực thuộc tỉnh, giữ vai trò trung tâm chính trị, kinh tế và văn hóa của tỉnh Lâm Đồng.	4	01	APR	1         	resource/images/page5_img1.jpg	NULL	NULL	NULL	NULL	NULL	NULL
