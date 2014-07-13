dbef70680ab28f25fbba75723ccb31cea5e5f358
Long Ngo <long.hispvietnam@gmail.com>
2014-07-13 11:57:08 +0700

    check session for account page

--- admin/account.aspx.cs
+++ admin/account.aspx.cs
@@ -10,7 +10,22 @@ public partial class admin_account : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+        if (Session["User"] != null && Session["User"] != "")
+        {
+            Account user = db.Accounts.Single(n => n.UserName == Session["User"].ToString());
+            if (user.Type == 1)
+            {
+                username.Text = Session["User"].ToString();
+            }
+            else
+            {
+                Response.Redirect("~/main.aspx");
+            }
+        }
+        else
+        {
+            Response.Redirect("~/main.aspx");
+        }
     }
     protected void btnSubmit_Click(object sender, EventArgs e)
     {
